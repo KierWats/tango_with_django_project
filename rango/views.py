9c5c21c0832d4aed7e6d5d0e79c2b5aa5f8ace69
Kieran Watson <2318086w@student.gla.ac.uk>
2020-02-12 18:25:03 +0000

    Chapter 10: passes tests

--- rango/views.py
+++ rango/views.py
@@ -9,7 +9,7 @@ from django.urls import reverse
 from rango.forms import UserForm, UserProfileForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-
+from datetime import datetime
 
 
 def index(request):
@@ -20,17 +20,30 @@ def index(request):
 	context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
 	context_dict['categories'] = category_list
 	context_dict['pages'] = page_list
+	
+	visitor_cookie_handler(request)
 
-	return render(request, 'rango/index.html', context=context_dict)
+	
+	response = render(request, 'rango/index.html', context=context_dict)
+	return response
 
 
 def about(request):
 
 	print(request.method)
 	print(request.user)
+	
+	if request.session.test_cookie_worked():
+		print("TEST COOKIE WORKED!")
+		request.session.delete_test_cookie()
+	
+	context_dict = {}
+	visitor_cookie_handler(request)
+	context_dict['visits'] = request.session['visits']
 
-
-	return render(request, 'rango/about.html', {})
+	response = render(request, 'rango/about.html', context=context_dict)
+	
+	return response
 	
 def show_category(request, category_name_slug):
 	context_dict = {}
@@ -162,4 +175,31 @@ def user_logout(request):
 @login_required
 def restricted(request):
 	return render(request, 'rango/restricted.html')
+
+def get_server_side_cookie(request, cookie, default_val=None):
+	val = request.session.get(cookie)
+	if not val:
+		val = default_val
+	return val
+
+	
+def visitor_cookie_handler(request):
+	visits = int(get_server_side_cookie(request, 'visits', '1'))
+	last_visit_cookie = get_server_side_cookie(request,
+		'last_visit',
+		str(datetime.now()))
+	last_visit_time = datetime.strptime(last_visit_cookie[:-7],
+		'%Y-%m-%d %H:%M:%S')
+		
+	if (datetime.now() - last_visit_time).days > 0:
+		visits = visits + 1
+	
+		request.session['last_visit'] = str(datetime.now())
+	else:
+
+		request.session['last_visit'] = last_visit_cookie
+	
+	request.session['visits'] = visits
+
+
 
